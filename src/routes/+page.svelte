64100dfdbe01005d26c055597df3325dc0109d56
akngs <jania902@gmail.com>
2023-02-01 20:15:14 +0900

    모니코 편집기 연동

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,52 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module" lang="ts">
+import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
+import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
+
+window.MonacoEnvironment = {
+  getWorker: function (_, label) {
+    switch (label) {
+      case 'typescript':
+      case 'javascript':
+        return new tsWorker()
+      default:
+        return new editorWorker()
+    }
+  },
+}
+</script>
+
+<script lang="ts">
+import * as monaco from 'monaco-editor'
+import { onMount } from 'svelte'
+
+let editorEl: HTMLDivElement
+let editor: monaco.editor.IStandaloneCodeEditor
+
+onMount(() => {
+  editor = monaco.editor.create(editorEl, {
+    value: "function hello() {\n\talert('Hello world!');\n}",
+    language: 'javascript',
+  })
+
+  console.log(editor)
+})
+</script>
+
+<div class="editor" bind:this={editorEl} />
+
+<style lang="postcss">
+:global {
+  & html,
+  & body {
+    width: 100%;
+    height: 100%;
+    padding: 0;
+    margin: 0;
+  }
+}
+
+.editor {
+  width: 100%;
+  height: 100%;
+}
+</style>
